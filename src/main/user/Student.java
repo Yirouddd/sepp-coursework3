373cbc9380f9ba1ce1ba359b514e2513639cf92a
coursework-1-mips-Yirouddd <coursework-1-mips-Yirouddd>
2026-03-17 19:35:28 +0000

    crated the skeleto of Student

---
+++ src/main/user/Student.java
@@ -0,0 +1,24 @@
+/**
+ * Student class represents a student with a name and a phoneNumber.
+ */
+public class Student {
+    private String name;
+    private int phoneNumber;
+
+    /**
+     * Constructs a new Student with the specified name and phoneNumber.
+     *
+     * @param name the name of the student
+     * @param phoneNumber the phone number of the student
+     */
+    public Student(String name, int phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+    
+    public void addBooing(Booking booking) {
+        // Implementation for adding a booking to the student
+    }
+
+   
+}

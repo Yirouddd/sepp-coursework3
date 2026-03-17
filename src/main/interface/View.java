2ad98e90592e7a16220ef70648ec697d24d7bfed
coursework-1-mips-Yirouddd <coursework-1-mips-Yirouddd>
2026-03-17 19:02:04 +0000

    add pubulic to each function

--- src/main/interface/View.java
+++ src/main/interface/View.java
@@ -11,35 +11,35 @@ public interface View {
      * @param inputPrompt the message to display to the user
      * @return the user's input as a String
      */
-    String getInput(String inputPrompt);
+    public String getInput(String inputPrompt);
     
     /**
      * Displays a success message to the user
      * @param successMessage the success message to display
      */
-    void displaySuccess(String successMessage);
+    public void displaySuccess(String successMessage);
     
     /**
      * Displays an error message to the user
      * @param errorMessage the error message to display
      */
-    void displayError(String errorMessage);
+    public void displayError(String errorMessage);
     
     /**
      * Displays a list of all performances
      * @param listOfPerformanceInfo collection of performance information strings
      */
-    void displayListOfPerformances(Collection<String> listOfPerformanceInfo);
+    public void displayListOfPerformances(Collection<String> listOfPerformanceInfo);
     
     /**
      * Displays details of a specific performance
      * @param performanceInfo the performance information to display
      */
-    void displaySpecificPerformance(String performanceInfo);
+    public void displaySpecificPerformance(String performanceInfo);
     
     /**
      * Displays a booking record
      * @param bookingRecord the booking record to display
      */
-    void displayBookingRecord(String bookingRecord);
+    public void displayBookingRecord(String bookingRecord);
 }

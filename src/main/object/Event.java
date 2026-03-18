bc9ab98b362e3688328c030cea6b72970cdd544c
ikatherahman <ikatherahman@gmail.com>
2026-03-18 14:15:37 +0000

    Add full skeleton to Event.java

--- src/main/object/Event.java
+++ src/main/object/Event.java
@@ -1,7 +1,65 @@
 /**
  * Event class
  */
-public class Event {
+public class Event{
+
+    private long eventID;
+    private String title; 
+    private EventType type;
+    private boolean isTicketed;
+
+    public Event() {
+        // TODO
+    }
+    public Performance createPerformance(long performanceID, LocalDateTime startDateTime, LocalDateTime endDateTime, Collection<String> performerNames, String venueAddress, int venueCapacity, boolean venueIsOutdoors, boolean venueIsSmoking, double ticketPrice) {
+        // TODO
+        return null;
+    }
+
+    public Performance getPerformanceByID(long performanceID) {
+        // TODO
+        return null;
+    }
+
+    public Collection<String> getInfoOfPerformancesOnDate(LocalDateTime searchDate) {
+        // TODO
+        return null;
+    }
+
+    public String getOrganiserName() {
+        // TODO
+        return null;
+    }
+
+    public String getOrganiserEmail() {
+        // TODO
+        return null;
+    }
+
+    public double getAverageRatingOfPerformances() {
+        // TODO
+        return 0.0;
+    }
+
+    public Collection<String> getAllPerformanceReviews() {
+        // TODO
+        return null;
+    }
+
+    public boolean hasPerformanceAtSameTimes(LocalDateTime startDateTime, LocalDateTime endDateTime) {
+        // TODO
+        return false;
+    }
+
+    public void addPerformance(Performance p) {
+        // TODO
+    }
+
+    public String toString() {
+        // TODO
+        return null;
+
+        
     public enum EventType {
         Music,
         Theatre,
@@ -9,4 +67,5 @@ public class Event {
         Movie,
         Sports
     }
+}
 }

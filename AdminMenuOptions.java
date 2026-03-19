5c0001c5654f1952642a98526c8ff64d7a005870
oleksandrabondarchuk <oleksandrabondarchuk46@gmail.com>
2026-03-19 02:07:44 +0000

    AdminMenuOptions
    to enums folder

---
+++ AdminMenuOptions.java
@@ -0,0 +1,9 @@
+/**
+ * Enumeration of menu options available to admin staff.
+ */
+public enum AdminMenuOptions {
+  LOGOUT,
+  SEARCH_FOR_PERFORMANCES,
+  VIEW_PERFORMANCE,
+  SPONSOR_PERFORMANCE
+}

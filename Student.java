0636cb38f747425347bc41b215c6243cd2e40879
oleksandrabondarchuk <oleksandrabondarchuk46@gmail.com>
2026-03-19 02:00:33 +0000

    Add files via upload
    Student class implementation

---
+++ Student.java
@@ -0,0 +1,39 @@
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Student class represents a student with a name.
+ */
+public class Student extends User {
+  private String name;
+  private int phoneNumber;
+  private List<Booking> bookings;
+
+  /**
+   * Constructs a new Student with the specified details.
+   * Email and password are inherited from abstract class User.
+   *
+   * @param email       the student email (from User class, used as username)
+   * @param password    the student password (from User abstract class)
+   * @param name        the name of the student
+   * @param phoneNumber the student's phone number
+   */
+  public Student(String email, String password, String name, int phoneNumber) {
+    super(email, password);
+    // name cannot be null or empty
+    assert name != null && !name.isEmpty();
+    assert phoneNumber > 0 : "Phone number should be valid.";
+
+    this.name = name;
+    this.phoneNumber = phoneNumber;
+    this.bookings = new ArrayList<>();
+  }
+
+  /**
+   * 
+   */
+  public List<Booking> addBooking(Booking booking) {
+    assert booking != null : "Non-existent booking cannot be added.";
+    bookings.add(booking);
+  }
+}

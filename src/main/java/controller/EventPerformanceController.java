38584bb17263447fcf1ae916b1c8c3a063ee735b
coursework-1-mips-Yirouddd <coursework-1-mips-Yirouddd>
2026-03-19 19:41:51 +0000

    update

---
+++ src/main/java/controller/EventPerformanceController.java
@@ -0,0 +1,56 @@
+package controller;
+
+import object.Event;
+import object.Performance;
+
+public class EventPerformanceController {
+
+    public Event createEvent() {
+
+        return null;
+    }
+
+    public void searchforPerformances() {
+
+    }
+
+    public void viewPerformance() {
+
+    }
+
+    public void cancelPerformance() {
+
+    }
+
+    private boolean checkIfSponsorshipPossible(Performance performance, int amount){
+
+        return false;
+    }
+
+    public void sponsorPerformance() {
+
+    }
+
+    private void addEvent(Event e){
+
+    }
+
+    private void addPerformance(Performance p){
+
+    }
+
+    private Event getEventByID(long eventID){
+
+        return null;
+    }
+
+    private Event getEventByTitle(String title){
+
+        return null;
+    }
+
+    private Performance getPerformanceById(long performanceID){
+
+        return null;
+    }
+}

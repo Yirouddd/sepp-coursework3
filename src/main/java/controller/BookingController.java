4281ebdc464eba8f2f0b182cd6722e2104c4c9e6
Olivia Bird <oliviabird@Olivias-MacBook-Pro.local>
2026-03-19 17:09:25 +0000

    BookingController skeleton

---
+++ src/main/java/controller/BookingController.java
@@ -0,0 +1,47 @@
+package controller;
+
+/**
+ * BookingController.java
+ */
+public class BookingController {
+    //main.user.StudentPreferences studentPreferences;
+
+    public static final long nextBookingNumber = "data.txt";
+
+    public void bookPerformance() {
+        // Implementation for booking of performance
+    }
+
+    public void reviewPerformance() {
+        // Implementation for reviewing performance
+    }
+
+    public void cancelBooking() {
+        // Implementation for cancelling a booking
+    }
+
+    private void addBooking(Booking b) {
+        // Implementation for adding a booking
+    }
+
+    private String getPerformanceByID(long performanceID) {
+        // Implementation for getting performanceID
+        return null; // Placeholder return value
+    }
+
+    private boolean checkIfBookingPossible(Performance performance, int numTickets) {
+        // Implementation for checking if a booking is possible
+        return false; // Placeholder return value
+    }
+
+    private String findBookingsByEventID(long eventID) {
+        // Implementation for finding a booking by its eventID
+        return null; // Placeholder return value
+    }
+
+    private String getBookingByNumber(long bookingNumber) {
+        // Implementation for finding a booking by its eventID
+        return null; // Placeholder return value
+    }
+
+}

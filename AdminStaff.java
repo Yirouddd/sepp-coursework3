e66d6ad9591f8534b4cb37ddf0d8225b36b9c885
oleksandrabondarchuk <oleksandrabondarchuk46@gmail.com>
2026-03-19 02:03:45 +0000

    Admin staff class
    implementation

---
+++ AdminStaff.java
@@ -0,0 +1,22 @@
+/**
+ * AdminStaff class represents an admin staff with a name.
+ */
+public class AdminStaff extends User {
+  private String name;
+
+  /**
+   * Constructs a new AdminStaff with the specified name.
+   * Email and password are inherited from abstract class User.
+   *
+   * @param email    the admin staff email (from User class, used as username)
+   * @param password the admin staff password (from User abstract class)
+   * @param name     the name of the admin staff
+   */
+  public AdminStaff(String email, String password, String name) {
+    super(email, password);
+    // name cannot be null or empty
+    assert name != null && !name.isEmpty();
+    this.name = name;
+
+  }
+}
